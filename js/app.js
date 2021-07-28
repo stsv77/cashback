03db0ec83f253997735cb70da734fdcbdb343693
Stsv <stsv77@yandex.ru>
2021-07-28 15:41:56 +0500

    calculator simple working version

--- js/app.js
+++ js/app.js
@@ -16,5 +16,42 @@ function calculateCashback(specialCategoryPurchases, otherCategoryPurchases) {
     };
 }
 
-const cashback = calculateCashback;
-console.log(cashback(10000, 15000));
+function handleSubmit(evt){
+    evt.preventDefault();
+
+    const specialAmountInputEl = document.getElementById('special-amount-input');
+    const specialAmount = Number(specialAmountInputEl.value);
+    if (Number.isNaN(specialAmount)){
+        // TODO: show error
+        return;
+    }
+    if (!Number.isFinite(specialAmount)){
+        // TODO: show error
+        return;
+    }
+
+    const otherAmountInputEl = document.getElementById('other-amount-form');
+    const otherAmount = otherAmountInputEl.value;
+    if (Number.isNaN(otherAmount)){
+        // TODO: show error
+        return;
+    }
+    if (!Number.isFinite(otherAmount)){
+        // TODO: show error
+        return;
+    }
+
+    const result = calculateCashback(specialAmount, otherAmount);
+    const specialCashbackEl = document.getElementById('special-cashback');
+    specialCashbackEl.textContent = `${result.specialCategoryCashback} руб.`;
+    const otherCashbackEl = document.getElementById('other-cashback');
+    otherCashbackEl.textContent = `${result.otherCategoryCashback} руб.`;
+    const totalCashbackEl = document.getElementById('total-cashback');
+    totalCashbackEl.textContent = `${result.totalCashback} руб.`;
+}
+
+const formEl = document.getElementById('cashback-form');
+formEl.onsubmit = handleSubmit;
+
+//const cashback = calculateCashback;
+//console.log(cashback(10000, 15000));
